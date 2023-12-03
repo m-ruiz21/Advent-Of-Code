309dfddbb365f3d3b2dd1eaf17522a8d991232b9
mateoruiz5171 <mateoruiz5171@gmail.com>
2023-12-03 12:56:59 -0600

    day1-part1

--- 2023/rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/mateoruiz5171/dev/Advent-Of-Code/2023/rust/day1/target/debug/day1: /home/mateoruiz5171/dev/Advent-Of-Code/2023/rust/day1/src/main.rs
--- 2023/rust/day1/target/debug/deps/day1-1faba5b46acaf912.d
+++
@@ -1,5 +0,0 @@
-/home/mateoruiz5171/dev/Advent-Of-Code/2023/rust/day1/target/debug/deps/day1-1faba5b46acaf912: src/main.rs
-
-/home/mateoruiz5171/dev/Advent-Of-Code/2023/rust/day1/target/debug/deps/day1-1faba5b46acaf912.d: src/main.rs
-
-src/main.rs:
